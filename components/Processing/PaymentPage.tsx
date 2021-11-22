6b1141fdc7d03c0d70fc9f0645bd8b89dd7e493e
Yury <y.arakelov@ips.su>
2021-11-22 16:42:07 +0300

    fixed commit

--- components/Processing/PaymentPage.tsx
+++ components/Processing/PaymentPage.tsx
@@ -1,91 +1,7 @@
 import React from "react";
 
 const PaymentPage = () => {
-<<<<<<< HEAD
-    const [toggleLanguage, setToggleLanguage] = useState(false)
-    const [toggleMultilanguage, setToggleMultilanguage] = useState(false)
 
-    const onHandleToggle = () => {
-        setToggleLanguage(toggleLanguage => !toggleLanguage)
-    }
-
-    const onHandleMultiToggle = () => {
-        setToggleMultilanguage(toggleMultilanguage => !toggleMultilanguage)
-    }
-
-    return (
-        <div className="subtab__content">
-        <table >
-            <tr className="content__table-colored">
-                <td>Логин</td>
-                <td>payment_page_login</td>
-            </tr>
-            <tr className='content__table'>
-                <td>Языки</td>
-                <td>
-                    <ul className='table-list'>
-                        <li
-                            className={`table-list__item ${!toggleLanguage ? '' : 'tab-choose'}`}
-                            onClick={onHandleToggle}
-                        >RU</li>
-                        <li
-                            className={`table-list__item ${!toggleLanguage ? 'tab-choose' : ''}`}
-                            onClick={onHandleToggle}
-                        >EN</li>
-
-                    </ul>
-                </td>
-            </tr>
-            <tr className="content__table-colored">
-                <td>Мультиязычность</td>
-                {/* Кошмарный переключатель мультиязычности*/}
-                <ul className='table-list'>
-                    <li
-                        className={`table-list__check ${!toggleMultilanguage ? 'table-list__circle': ''}`}
-                        onClick={onHandleMultiToggle}
-                    >
-
-                    </li>
-                    <li
-                        className={`table-list__cross ${!toggleMultilanguage ? '': 'table-list__circle'}`}
-                        onClick={onHandleMultiToggle}
-                    >
-
-                    </li>
-                </ul>
-            </tr>
-            <tr className='content__table'>
-                <td>Логин финальной страницы</td>
-                <td>finish_page_login</td>
-            </tr>
-            <tr className="content__table-colored">
-                <td>Видимые доп. параметры заказа</td>
-                <td>show_param_list_mapping</td>
-            </tr>
-            <tr className='content__table'>
-                <td>Количество попыток оплаты</td>
-                <td>payment_attempts</td>
-            </tr>
-            <tr className="content__table-colored">
-                <td>Продолжительность сессии</td>
-                <td>session_timeout_secs</td>
-            </tr>
-            <tr className='content__table'>
-                <td>Двухстадийный заказ автома</td>
-                <td>завершится через:  м.</td>
-            </tr>
-            <tr className="content__table-colored">
-                <td>URL возврата</td>
-                <td>backup_url</td>
-            </tr>
-            <tr className='content__table'>
-                <td>URL ошибки</td>
-                <td>fail_url</td>
-            </tr>
-        </table>
-        </div>
-    )
-=======
   return (
     <table className="subtab__content">
       <tbody>
@@ -141,7 +57,6 @@ const PaymentPage = () => {
       </tbody>
     </table>
   );
->>>>>>> master
 };
 
 export default PaymentPage;
