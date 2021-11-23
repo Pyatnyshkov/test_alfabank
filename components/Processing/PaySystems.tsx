fdb1eced3cbe3c4bd5ea4fedd871bb50cb4a04bb
Николай Пятнышков <n.piatnyshkov@egopay.ru>
2021-11-23 18:31:43 +0300

    fix

--- components/Processing/PaySystems.tsx
+++ components/Processing/PaySystems.tsx
@@ -1,128 +1,44 @@
 import React, { useState } from "react";
-import {useAppSelector} from "../../helpers/redux-hooks";
-import app from "../../store/reducers/app";
+import { useAppSelector } from "../../helpers/redux-hooks";
 
+interface paymentSystems {
+  [key: string]: string | undefined;
+}
 
 const PaySystems = () => {
-  const [toggleState, setToggleState] = useState(1);
-  const merchant = useAppSelector(state => state.app.merchant)
-  const toggleTab = (index: any) => {
-    setToggleState(index);
+  const [paySystem, setPaySystem] = useState("");
+  const merchant = useAppSelector(state => state.app.merchant);
+  const toggleTab = (paySystem: string) => {
+    setPaySystem(paySystem);
   };
 
-
-  const paymentSystems: any = {
+  const paymentSystems: paymentSystems = {
     mir_3ds: merchant.mir_3ds_id,
     mc_3ds: merchant.master_card_3ds_id,
     visa_3ds: merchant.visa_3ds_id,
     jcb_3ds: merchant.jcb_3ds_id,
     cup_3ds: merchant.cup_3ds_id,
     amex_3ds: merchant.amex_3ds_id
-  }
-
-  const apply = Object.keys(paymentSystems).filter(x => x !== undefined)
-
-  const checkData = {
-    checkMirData: function () {
-      if (merchant.mir_3ds_id) {
-        return merchant.mir_3ds_id
-      } else {
-        return '-'
-      }
-    },
-    checkMcData: function () {
-      if(merchant.master_card_3ds_id){
-        return merchant.master_card_3ds_id
-      } else {
-        return '-'
-      }
-    },
-    checkVisaData: function () {
-      if(merchant.visa_3ds_id){
-        return merchant.visa_3ds_id
-      } else {
-        return '-'
-      }
-    },
-    checkJcbData: function () {
-      if(merchant.jcb_3ds_id){
-        return merchant.jcb_3ds_id
-      } else {
-        return '-'
-      }
-    },
-    checkCupData: function () {
-      if(merchant.amex_3ds_id){
-        return merchant.cup_3ds_id
-      } else {
-        return '-'
-      }
-    },
-    checkAmexData: function () {
-      if(merchant.amex_3ds_id){
-        return merchant.amex_3ds_id
-      } else {
-        return '-'
-      }
-    }
-  }
-  const getContent = () => {
-    switch (toggleState) {
-      case 1:
-        return checkData.checkMirData()
-      case 2:
-        return  checkData.checkMcData();
-      case 3:
-        return checkData.checkVisaData();
-      case 4:
-        return checkData.checkJcbData();
-      case 5:
-        return checkData.checkCupData();
-      case 6:
-        return checkData.checkAmexData();
-    }
   };
+
+  const apply = Object.keys(paymentSystems).filter(x => x);
 
   return (
     <div className="subtab__content">
       <div className="bloc-tabs">
-
-        {apply.map((paymentSystem: any, index) => (
-            <button
-              key={index}
-              className={toggleState === paymentSystem ? "subtabs active-tabs" : "subtabs"}
-              onClick={() => toggleTab(paymentSystem)}
-            />
+        {apply.map((paymentSystem: string, index: number) => (
+          <button
+            key={index}
+            className={
+              paySystem === paymentSystem ? "subtabs active-tabs" : "subtabs"
+            }
+            onClick={() => toggleTab(paymentSystem)}
+          />
         ))}
-
-        {/*<button*/}
-        {/*  className={toggleState === 1 ? "subtabs active-tabs" : "subtabs"}*/}
-        {/*  onClick={() => toggleTab(1)}*/}
-        {/*/>*/}
-        {/*<button*/}
-        {/*  className={toggleState === 2 ? "subtabs active-tabs" : "subtabs"}*/}
-        {/*  onClick={() => toggleTab(2)}*/}
-        {/*/>*/}
-        {/*<button*/}
-        {/*  className={toggleState === 3 ? "subtabs active-tabs" : "subtabs"}*/}
-        {/*  onClick={() => toggleTab(3)}*/}
-        {/*/>*/}
-        {/*<button*/}
-        {/*  className={toggleState === 4 ? "subtabs active-tabs" : "subtabs"}*/}
-        {/*  onClick={() => toggleTab(4)}*/}
-        {/*/>*/}
-        {/*<button*/}
-        {/*  className={toggleState === 5 ? "subtabs active-tabs" : "subtabs"}*/}
-        {/*  onClick={() => toggleTab(5)}*/}
-        {/*/>*/}
-        {/*<button*/}
-        {/*  className={toggleState === 6 ? "subtabs active-tabs" : "subtabs"}*/}
-        {/*  onClick={() => toggleTab(6)}*/}
-        {/*/>*/}
       </div>
       <div className="content-tabs">
         <strong>ID: </strong>
-        <div className="content">{getContent()}</div>
+        <div className="content">{merchant[paySystem]}</div>
       </div>
     </div>
   );
