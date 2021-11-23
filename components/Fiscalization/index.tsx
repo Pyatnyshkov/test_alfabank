2050f3c2778e1e4bd9a7fd45b903018c8d2876fd
Yan <y.aliasov@ips.su>
2021-11-23 21:19:00 +0300

    fiscalization

--- components/Fiscalization/index.tsx
+++ components/Fiscalization/index.tsx
@@ -1,7 +1,48 @@
 import React, { FC } from "react";
+import {useAppSelector} from "../../helpers/redux-hooks";
 
 const Fiscalization: FC = () => {
-    return <section className="subtab__content">Fiscalization</section>;
+    const merchant:any = useAppSelector(state => state.app.merchant);
+    const CAN_CREATE_OFD_RECEIPTS_MANUALLY = merchant.options?.includes('CAN_CREATE_OFD_RECEIPTS_MANUALLY') ? 'content__table-check' : 'content__table-uncheck';
+
+    return <section className="subtab__content">
+        <table className="subtab__content">
+			<tbody>
+				<tr className="content__table-colored">
+					<td>
+						<span className="content__table-span">ИНН</span>
+					</td>
+					<td>
+						<span className="content__table-span">{merchant.merchant_inn}</span>
+					</td>
+				</tr>
+                <tr className="content__table">
+					<td>
+						<span className="content__table-span">КПП</span>
+					</td>
+					<td>
+						<span className="content__table-span">{merchant.merchant_kpp}</span>
+					</td>
+				</tr>
+                <tr className="content__table-colored">
+					<td>
+						<span className="content__table-span">ОГРН</span>
+					</td>
+					<td>
+						<span className="content__table-span">{merchant.merchant_ogrn}</span>
+					</td>
+				</tr>
+                <tr className="content__table">
+					<td>
+						<span className="content__table-span">Разрешено формирование фискальных чеков</span>
+					</td>
+					<td>
+						<span className={`content__table-span ${CAN_CREATE_OFD_RECEIPTS_MANUALLY}`}></span>
+					</td>
+				</tr>
+            </tbody>
+        </table>
+    </section>;
 };
 
 export default Fiscalization;
