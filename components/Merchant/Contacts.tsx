8e944f0bbd07aab6d2617846516ecd6017ebfdb2
Yan <y.aliasov@ips.su>
2021-11-23 14:01:37 +0300

    merchant-contacts

--- components/Merchant/Contacts.tsx
+++ components/Merchant/Contacts.tsx
@@ -1,34 +1,69 @@
 import React, { FC } from "react";
-import {useAppSelector} from "../../helpers/redux-hooks";
+import { useAppSelector } from "../../helpers/redux-hooks";
 
 const Contacts: FC = () => {
-    const app = useAppSelector(state => state.app)
-    return <div className="subtab__content">
-        <table>
+    const app = useAppSelector(state => state.app);
+    const merchant = app.merchant;
+    return <table className="subtab__content">
             <tbody>
                 <tr className="content__table-colored">
-                    <td>URL</td>
-                    <td>{app.merchant.merchant_url}</td>
+                    <td>
+                        <span className="content__table-span">Адрес сайта (URL)</span>
+                    </td>
+                    <td>
+                        <span className="content__table-span">{merchant.merchant_url}</span>
+                    </td>
                 </tr>
-                <tr className='content__table'>
-                    <td>Оферта</td>
-                    <td>{app.merchant.agreement_url}</td>
+                <tr className="content__table">
+                    <td>
+                        <span className="content__table-span">Адрес оферты</span>
+                    </td>
+                    <td>
+                        <span className="content__table-span">{merchant.agreement_url}</span>
+                    </td>
                 </tr>
-                <tr className='content__table'>
-                    <td>Emails</td>
-                    <td>{app.merchant.merchant_emails}</td>
+                <tr className="content__table-colored">
+                    <td>
+                        <span className="content__table-span">Адреса почты</span>
+                    </td>
+                    <td>
+                        <span className="content__table-span">{merchant.merchant_emails}</span>
+                    </td>
                 </tr>
-                <tr className='content__table'>
-                    <td>Номер телефона</td>
-                    <td>{app.merchant.merchant_phone}</td>
+                <tr className="content__table">
+                    <td>
+                        <span className="content__table-span">Фактический адрес мерчанта</span>
+                    </td>
+                    <td>
+                        <span className="content__table-span">{merchant.merchant_actual_address}</span>
+                    </td>
                 </tr>
-                <tr className='content__table'>
-                    <td>Email для связи</td>
-                    <td>{app.merchant.merchant_feedback_email}</td>
+                <tr className="content__table-colored">
+                    <td>
+                        <span className="content__table-span">Номер телефона мерчанта</span>
+                    </td>
+                    <td>
+                        <span className="content__table-span">{merchant.merchant_phone}</span>
+                    </td>
+                </tr>
+                <tr className="content__table">
+                    <td>
+                        <span className="content__table-span">Адрес для обратной связи</span>
+                    </td>
+                    <td>
+                        <span className="content__table-span">{merchant.merchant_feedback_email}</span>
+                    </td>
+                </tr>
+                <tr className="content__table-colored">
+                    <td>
+                        <span className="content__table-span">Описание мерчанта</span>
+                    </td>
+                    <td>
+                        <span className="content__table-span">{merchant.description}</span>
+                    </td>
                 </tr>
             </tbody>
         </table>
-    </div>
 };
 
 export default Contacts;
